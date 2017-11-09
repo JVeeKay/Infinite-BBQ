a061f5c6a23fe2c8539f80b10ab50a9a9f1b057a
Jaimus VanKieran <spekmoo@hotmail.com>
2017-11-08 21:59:26 -0700

    menu page and home page complete except footer

--- js/main.js
+++ js/main.js
@@ -5,3 +5,14 @@ $(document).ready(function ($) {
         $('.panel').not($(this).next()).slideUp('slow');
     });
 }(jQuery));
+
+$(document).ready("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(5000)
+    .next()
+    .fadeIn(5000)
+    .end()
+    .appendTo('#slideshow');
+},  5000);
